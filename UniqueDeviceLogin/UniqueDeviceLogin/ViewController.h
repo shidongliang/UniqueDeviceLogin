0a56ef4c6b11b19df18bd795cdb230b7693c40fb
shidongliang <xiaoxinmao2012@gmail.com>
2016-05-08 23:52:26 +0800

    basic setup
    start a new project to simulate Unique device login

---
+++ UniqueDeviceLogin/UniqueDeviceLogin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UniqueDeviceLogin
+//
+//  Created by 天 王 on 16/5/8.
+//  Copyright © 2016年 天 王. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UniqueDeviceLogin/UniqueDeviceLogin/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UniqueDeviceLogin
+//
+//  Created by 天 王 on 16/5/8.
+//  Copyright © 2016年 天 王. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
